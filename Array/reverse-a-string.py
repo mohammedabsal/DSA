24b7706ef8af22ae09c83cd5578c67e4960fd89b
mohammedabsal <mohammedabsal9943@gmail.com>
2025-10-05 17:17:21 +0530

    added

---
+++ Array/reverse-a-string.py
@@ -0,0 +1,4 @@
+text="Mohammed Absal S"
+def reverse_text(text):
+    return text[::-1]
+print(reverse_text(text))
---
+++ Array/test.py
@@ -0,0 +1,21 @@
+# Using double quotes to include an apostrophe
+message_double = "I'm a happy programmer."
+
+# Using single quotes requires escaping the apostrophe
+message_single = 'I\'m a happy programmer.'
+myfloat=10.0
+if isinstance(myfloat, float):
+    print(True)
+print(message_double)
+print(message_single)
+
+# Python's order of operations, also known as PEMDAS (Parentheses, Exponents, Multiplication and Division, Addition and Subtraction).
+# number = (1 + 2 * 3 / 4)
+# print(number)
+
+lotsofhellos = "hello" * 10
+print(lotsofhellos,end="\n")
+
+astring = "Hello world!"
+afewwords = astring.split(" ")
+print(afewwordsy)
